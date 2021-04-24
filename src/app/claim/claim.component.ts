7948c067fea5c482b0a314981ccd92eea4603581
Yash <penteyash@gmail.com>
2021-04-24 19:39:07 +0530

    made changes to claim component

--- src/app/claim/claim.component.ts
+++ src/app/claim/claim.component.ts
@@ -9,13 +9,22 @@ import { ClaimService } from '../claim.service';
 })
 export class ClaimComponent implements OnInit {
 
-  constructor(private service: ClaimService) { }
+  constructor(private service: ClaimService) {
+    this.farmer.id = parseInt(sessionStorage.getItem('farmerId'));
+    this.service.fetchInsurance(this.farmer).subscribe(data => {
+      alert(JSON.stringify(data));
+      this.sample = data;
+    })
+
+   }
 
   ngOnInit(): void {
   }
 
   claim: Claim = new Claim();
   message: string;
+  sample : any;
+  farmer: Farmer = new Farmer();
 
   claiming(f:NgForm) {
     this.claim.farmer.id = parseInt(sessionStorage.getItem('farmerId'));
